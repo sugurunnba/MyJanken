82fca2bff6ff4a155a4c612c9bb93265c6368c55
sugurunnba <sugurunnba7@gmail.com>
2022-03-27 08:26:53 +0900

    answerNumberとnewAnswerNumberで前回の手と比較後、同じでなければ処理実行する。 icon画像設定

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+//            スペースを追加
+            Spacer()
+            
             if answerNumber == 0 {
                 Text("これからじゃんけんをします")
             } else if answerNumber == 1 {
@@ -21,30 +24,51 @@ struct ContentView: View {
                     .resizable()
         //            アスペクト比(縦横比)の調整
                     .aspectRatio(contentMode: .fit)
+                Spacer()
     //            じゃんけんの種類を指定
                 Text("グー")
+                    .padding(.bottom)
             } else if answerNumber == 2 {
                 Image("choki")
         //            画像を画面内に収めるようリサイズする
                     .resizable()
         //            アスペクト比(縦横比)の調整
                     .aspectRatio(contentMode: .fit)
+                Spacer()
     //            じゃんけんの種類を指定
                 Text("チョキ")
+                    .padding(.bottom)
             } else {
                 Image("pa")
         //            画像を画面内に収めるようリサイズする
                     .resizable()
         //            アスペクト比(縦横比)の調整
                     .aspectRatio(contentMode: .fit)
+                Spacer()
     //            じゃんけんの種類を指定
                 Text("パー")
+                    .padding(.bottom)
             }
             
             Button(action:{
-                answerNumber = Int.random(in: 1...3)
+                var newAnswerNumber = 0
+                
+                repeat{
+                    newAnswerNumber = Int.random(in: 1...3)
+                } while answerNumber == newAnswerNumber
+                answerNumber = newAnswerNumber
             }) {
                 Text("じゃんけんをする!")
+//                 テキストの下に余白を設定
+                    .padding(.bottom)
+//                　画面幅いっぱいに表示
+                    .frame(maxWidth: .infinity)
+//                　iOSが提供している組み込みフォントサイズ(既に規定されているフォントサイズ)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+//                　文字色変更
+                    .foregroundColor(Color.white)
             }
         }
         
